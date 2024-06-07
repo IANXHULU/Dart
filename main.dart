fbaf66df7fcc4d994af066ceae1b4a1d0b8fb029
Xhulu <65667993+IANXHULU@users.noreply.github.com>
2024-06-07 05:17:31 +0200

    Initial commit

---
+++ main.dart
@@ -0,0 +1,24 @@
+void printAssignment(){
+  // this line assigns value of string data type to the variable name and country
+  String name = "Ian";
+  String country = "zambia";
+  
+  //this line assings value of data type num to variable age and height
+  num age = 20;
+  num height = 1.74;
+
+
+//prints out all the values 
+  print("$name");
+  print("$country");
+  print("$height");
+  print("$age");
+  print("Hello $name, you are $age years Old, your live in $country and your height is $height");
+}
+
+
+
+void main() {
+  printAssignment();
+}
+
